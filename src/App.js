29d3a9fb8542a5a3138552212efed61c9c217127
Rodrigo Nogueira <rdg6design@gmail.com>
2021-02-19 02:45:39 -0300

    Rotas privadas - react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import 'react-perfect-scrollbar/dist/css/styles.css';
-import React from 'react';
+import React, { useContext } from 'react';
 import { useRoutes } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core';
 import GlobalStyles from 'src/components/GlobalStyles';
 import 'src/mixins/chartjs';
 import theme from 'src/theme';
 import routes from 'src/routes';
+import { Context } from 'src/context/AuthContext';
 
 const App = () => {
-  const routing = useRoutes(routes);
+  const { signed } = useContext(Context);
+
+  const routing = useRoutes(routes(signed));
 
   return (
     <ThemeProvider theme={theme}>
