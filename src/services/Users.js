47410c513e199708a81cfc44393b6de15ff6a376
Rodrigo Nogueira <rdg6design@gmail.com>
2021-02-20 12:06:21 -0300

    Iniciando crud

--- src/services/Users.js
+++ src/services/Users.js
@@ -1,7 +1,39 @@
-import axios from 'src/axios'
+import axios from "../axios";
 
 const getAll = () => {
-    return axios.get("/users");
-  };
-    
-export default getAll;
+  return axios.get("/tutorials");
+};
+
+const get = id => {
+  return axios.get(`/tutorials/${id}`);
+};
+
+const create = data => {
+  return axios.post("/tutorials", data);
+};
+
+const update = (id, data) => {
+  return axios.put(`/tutorials/${id}`, data);
+};
+
+const remove = id => {
+  return axios.delete(`/tutorials/${id}`);
+};
+
+const removeAll = () => {
+  return axios.delete(`/tutorials`);
+};
+
+const findByTitle = title => {
+  return axios.get(`/tutorials?title=${title}`);
+};
+
+export default {
+  getAll,
+  get,
+  create,
+  update,
+  remove,
+  removeAll,
+  findByTitle
+};
